f3b3bca6e3cfcad7eaaf47ef22640959841fcd2e
ngochaha <ttn6246323@gmail.com>
2020-02-17 19:58:11 +0700

    Hello Ngoc

--- vjp-group-1/Program.cs
+++ vjp-group-1/Program.cs
@@ -6,7 +6,7 @@ namespace vjp_group_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Ngoc");
         }
     }
 }
