19105e798f0af7e097771765e067ead73ddf4f9c
hoint <hointit@gmail.com>
2020-02-17 19:45:36 +0700

    innit project

---
+++ vjp-group-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vjp_group_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
